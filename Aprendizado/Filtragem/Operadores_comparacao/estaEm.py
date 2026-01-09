f3f7bd4b5f21292ddec871b3910efd0593599f29
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-01-09 16:34:15 -0300

    Esta em (IN)

---
+++ Aprendizado/Filtragem/Operadores_comparacao/estaEm.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client = MongoClient(os.getenv("MONGO_URI"))
+
+def testar_conexao():
+    try:
+        client.admin.command('ping')
+        print("Conexão estabelecida com sucesso!")
+    except Exception as e:
+        print(f"Erro ao conectar: {e}")
+        exit()
+
+def esta():
+    db = client['Escola_Banco']
+    collection = db['alunos']
+    query = {'nome': {'$in': ['Arthur', 'Josias']}}
+    all_documents = collection.find(query)
+    for i in all_documents:
+        print(i)    
+
+if __name__ == "__main__":
+    testar_conexao()
+    esta()

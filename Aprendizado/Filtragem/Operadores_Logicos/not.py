2a25829114e64b0a6a4e63c7447b0eece5d11fe0
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-01-16 19:06:54 -0300

    Not

---
+++ Aprendizado/Filtragem/Operadores_Logicos/not.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client = MongoClient(os.getenv("MONGO_URI"))
+
+def testar_conexao():
+    try:
+        client.admin.command('ping')
+        print("Conexão estabelecida com sucesso!")
+    except Exception as e:
+        print(f"Erro ao conectar: {e}")
+        exit()
+
+def operador_not():
+    db = client['Escola_Banco']
+    colecao = db['alunos']
+    query = {'idade': {'$not': {'$eq': 19}}}
+    filter = colecao.find(query)
+    for i in filter:
+        print(i)
+
+if __name__ == "__main__":
+    testar_conexao()
+    operador_not()

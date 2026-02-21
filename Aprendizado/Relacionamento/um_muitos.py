afcf8d292ba7a007fd74d0f42028b1884bb45cef
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-02-20 22:27:08 -0300

    Relacionamento 1 - N

---
+++ Aprendizado/Relacionamento/um_muitos.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+cliente = MongoClient(os.getenv("MONGO_URI"))
+db = cliente['Farmacia']
+collection = db['Vendas']
+
+def insercao_um_para_n_exemplo():
+    query = {
+        '_id': 'f01',
+        'nome_prod': 'Dorflex',
+        'fornecedores': [
+            {'nome': 'Empresa X'},
+            {'nome': 'Empresa Y'}
+        ]
+        }
+    collection.insert_one(query)
+    res = collection.find({})
+    for i in res:
+        print(i)
+
+if __name__ == "__main__":
+    print("Isto é um exemplo de um para muitos (1 - n), pois um produto pode ter varios fornecedores diferentes, ou seja 1 produto tem n fornecedores, assim adicionamos dentro do produto uma lista de fornecedores de tal item.")
+    insercao_um_para_n_exemplo()

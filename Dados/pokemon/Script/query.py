b8d58bb4e2dc7ef921a9b4d41dd1fcb20ff662a6
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-03-06 16:25:57 -0300

    See legendary and count

---
+++ Dados/pokemon/Script/query.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+cliente = MongoClient(os.getenv("MONGO_URI"))
+
+
+db = cliente["pokemon_center"]
+collection  = db["pokemon"]
+
+def lendarios_count():
+    queryLendario = {"legendary": {"$eq": True}}
+    res = collection.find(queryLendario)
+    contagem = collection.count_documents(queryLendario)
+    for i in res:
+        print(i)
+    return f"Total: {contagem}"
+
+print(lendarios_count())

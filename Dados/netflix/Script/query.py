973b86335f1cba5251092c4a182034c0b3511b4a
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-05-01 14:20:18 -0300

    Filter release year

---
+++ Dados/netflix/Script/query.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient,ASCENDING
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+cliente = MongoClient(os.getenv("MONGO_URI"))
+
+
+db = cliente["Netflix"]
+collection  = db["attractive"]
+
+def create_index_release_year():
+    collection.create_index([("release_year",1)])
+
+def find_release_year():
+    res = collection.find({"release_year": {"$lt": 2010}})
+    for i in res:
+        print(i)
+

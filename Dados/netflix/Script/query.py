57ec52c6a0024e8cc2c536e9f8eccb1fc7c9bcca
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-05-22 17:12:16 -0300

    Simple update

--- Dados/netflix/Script/query.py
+++ Dados/netflix/Script/query.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient,ASCENDING
 from dotenv import load_dotenv
+import datetime
 import os
 
 load_dotenv()
@@ -29,3 +30,9 @@ def simple_delete(title: str):
 def simple_insert():
     collection.insert_one({"type": "TV Show", "title": "The Boys", "date_added": {"$date": "2019-07-26T00:00:00.000Z"}, "release_year": 2019, "rating": "TV-MA", "duration": "4 Seasons", "description": "A fun and irreverent take on what happens when superheroes abuse their superpowers rather than use them for good.", "_id": 81045239, "cast": ["Karl Urban", "Jack Quaid", "Antony Starr", "Erin Moriarty"], "countries": ["United States"], "directors": ["Eric Kripke"], "listed_in": ["Action TV Shows", "TV Sci-Fi & Fantasy", "TV Dramas"]})
     return "Sucess add"
+
+def simple_update(title: str):
+    query = {"title": title}
+    new_param = {"$set": {"release_year": datetime.datetime.now()}}
+    collection.update_one(query,new_param)
+    return "Sucess update"

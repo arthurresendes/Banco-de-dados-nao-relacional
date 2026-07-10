75cf65d70b1494f73e898b32bb7a6e19e804af76
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-07-10 10:22:50 -0300

    Top 5 de varios tipos

--- Dados/netflix/netflix-front/src/components/NavBar.jsx
+++ Dados/netflix/netflix-front/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     const location = useLocation()
 
     const links = [
-        { to: '/', label: 'Top 5 Netflix' },
+        { to: '/', label: 'Top 5 filmes/séries por tipo' },
         { to: '/seculo-1900', label: 'Filmes Anos 1900' },
         { to: '/find-name', label: 'Busca por nome' },
         { to: '/find-actor', label: 'Busca por ator' },

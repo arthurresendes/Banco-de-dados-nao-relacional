a5af805670552a0b7df085a06eff1702b237c3b3
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-07-31 11:42:09 -0300

    Ajuste mensagem de erro

--- Dados/netflix/netflix-front/src/api/patchAtualizarCast.js
+++ Dados/netflix/netflix-front/src/api/patchAtualizarCast.js
@@ -13,7 +13,7 @@ export const atualizarCast = async (dados) => {
     throw new Error("Filme não encontrado");
   }
 
-  if (res.status === 409) {
+  if (res.status === 429) {
     throw new Error("Erro ao solicitar requisição. Aguarde!");
   }
 
